c4595c1fe89c1f5237913a4e7068a9ad4de9ba15
abhishekkhati-qmf <abhishekkhati6235@gmail.com>
2025-08-12 20:02:34 +0530

    final

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -20,6 +20,11 @@ export const register = async (req, res) => {
     }
     const hashedPassword = await bcrypt.hash(password, 10);
     await User.create({ name, email, password: hashedPassword });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "none",
+    });
     return res.status(201).json({
       success: true,
       message: "Account created successfully.",
@@ -29,9 +34,9 @@ export const register = async (req, res) => {
     return res.status(500).json({
       success: false,
       message: "Failed to register",
-    })
+    });
   }
-}
+};
 
 export const login = async (req, res) => {
   try {
@@ -40,7 +45,7 @@ export const login = async (req, res) => {
       return res.status(400).json({
         success: false,
         message: "All fields are required.",
-      })
+      });
     }
     const user = await User.findOne({ email });
     if (!user) {
@@ -56,75 +61,80 @@ export const login = async (req, res) => {
         message: "Incorrect email or password",
       });
     }
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "none",
+    });
     generateToken(res, user, `Welcom back ${user.name}`);
   } catch (error) {
     console.log(error);
     return res.status(500).json({
       success: false,
       message: "Failed to login",
-    })
+    });
   }
-}
+};
 
 export const logout = async (_, res) => {
   try {
     return res.status(200).cookie("token", "", { maxAge: 0 }).json({
       message: "Logged out successfully",
       success: true,
-    })
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).json({
       success: false,
       message: "Failed to logout",
-    })
+    });
   }
-}
+};
 
 export const getUserProfile = async (req, res) => {
   try {
     const userId = req.id;
     const user = await User.findById(userId)
-  .select("-password")
-  .populate({
-    path: "enrolledCourse",
-    populate: {
-      path: "creator",
-      select: "name photoUrl", 
-    }
-  });
+      .select("-password")
+      .populate({
+        path: "enrolledCourse",
+        populate: {
+          path: "creator",
+          select: "name photoUrl",
+        },
+      });
 
     if (!user) {
       return res.status(404).json({
         message: "Profile not found",
         success: false,
-      })
+      });
     }
     return res.status(200).json({
       success: true,
-      user
-    })
+      user,
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).json({
       success: false,
-      message: "Failed to load user"
-    })
+      message: "Failed to load user",
+    });
   }
-}
+};
 
 export const updateProfile = async (req, res) => {
   try {
     const userId = req.id;
-    const { name,role } = req.body;
+    const { name, role } = req.body;
     const profilePhoto = req.file;
 
     const user = await User.findById(userId);
     if (!user) {
       return res.status(404).json({
         message: "User not found",
-        success: false
-      })
+        success: false,
+      });
     }
     // extract public id of the old image from the url if it exists
     if (user.photoUrl) {
@@ -135,20 +145,20 @@ export const updateProfile = async (req, res) => {
     //upload new photo
     const cloudResponse = await uploadMedia(profilePhoto.path);
     const photoUrl = cloudResponse.secure_url;
-    const updatedData = { name,role,photoUrl };
+    const updatedData = { name, role, photoUrl };
     const updatedUser = await User.findByIdAndUpdate(userId, updatedData, {
       new: true,
     }).select("-password");
     return res.status(200).json({
       success: true,
       user: updatedUser,
-      message: "Profile updated successfully"
-    })
+      message: "Profile updated successfully",
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).json({
       success: false,
       message: "Failed to update profile",
-    })
+    });
   }
-}
+};
